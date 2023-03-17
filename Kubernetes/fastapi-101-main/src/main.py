14ae696a3027e241c7925259e75ac778fca7e8fe
Kacperek0 <47487512+Kacperek0@users.noreply.github.com>
2023-03-17 21:29:28 +0100

    Update main.py

--- Kubernetes/fastapi-101-main/src/main.py
+++ Kubernetes/fastapi-101-main/src/main.py
@@ -115,4 +115,4 @@ def delete_post(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=80, reload=True)
