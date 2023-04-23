e0d2ca09bf2bbf50b61682c0c823a98a480c5c3e
Kacperek0 <kacper.szczepanek@gmail.com>
2023-04-23 09:45:46 +0200

    update 2023-04-23-09-45-46

---
+++ Hardening/hardening/cramfs_hardening.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+{
+   l_mname="cramfs" # set module name
+   if ! modprobe -n -v "$l_mname" | grep -P -- '^\h*install \/bin\/(true|false)'; then
+      echo -e " - setting module: \"$l_mname\" to be not loadable"
+      echo -e "install $l_mname /bin/false" >> /etc/modprobe.d/"$l_mname".conf
+   fi
+   if lsmod | grep "$l_mname" > /dev/null 2>&1; then
+      echo -e " - unloading module \"$l_mname\""
+      modprobe -r "$l_mname"
+   fi
+   if ! grep -Pq -- "^\h*blacklist\h+$l_mname\b" /etc/modprobe.d/*; then
+      echo -e " - deny listing \"$l_mname\""
+      echo -e "blacklist $l_mname" >> /etc/modprobe.d/"$l_mname".conf
+   fi
+}
