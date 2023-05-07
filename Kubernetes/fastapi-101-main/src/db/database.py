ee8dc8e544a9df5e13660f8edfac6d25c2b31518
Kacperek0 <kacper.szczepanek@gmail.com>
2023-05-07 14:14:25 +0200

    container

--- Kubernetes/fastapi-101-main/src/db/database.py
+++ Kubernetes/fastapi-101-main/src/db/database.py
@@ -4,12 +4,11 @@ import sqlalchemy as sql
 import sqlalchemy.ext.declarative as declarative
 import sqlalchemy.orm as orm
 
-from dotenv import load_dotenv
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 
-load_dotenv()
-
-
-SQLALCHEMY_DATABASE_URL = f"sqlite:///./database.db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/postgres"
 
 engine = sql.create_engine(
     SQLALCHEMY_DATABASE_URL
