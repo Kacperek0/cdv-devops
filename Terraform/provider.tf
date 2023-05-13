b2b2eb4f3ef1be727b5ecb568103e62fd0512b97
CDV-KK <kkesy1@edu.cdv.pl>
2023-05-13 11:29:38 +0200

    terraform provider fix

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -6,8 +6,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "cdv-tfstate-rg"
-    storage_account_name = "cdvftstatekacper"
+    # resource_group_name  = "cdv-tfstate-rg"
+    resource_group_name = "cdv-tfstate-fg"
+    # storage_account_name = "cdvftstatekacper"
+    storage_account_name = "cdvtfstatekk"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
