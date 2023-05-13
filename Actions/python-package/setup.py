ed6d9458523249d125d01ab3d09b6cf4df940544
Kacperek0 <kacper.szczepanek@gmail.com>
2023-05-13 15:02:17 +0200

    deploy package

--- Actions/python-package/setup.py
+++ Actions/python-package/setup.py
@@ -14,8 +14,8 @@ with open(path.join(HERE, 'readme.md'), encoding='utf-8') as f:
 
 # This call to setup() does all the work
 setup(
-    name="klarity-connector",
-    version="0.1.5",
+    name="cdv-connector",
+    version="0.1.1",
     description="Python connector to GraphQL API of Klarity",
     long_description=long_description,
     long_description_content_type="text/markdown",
