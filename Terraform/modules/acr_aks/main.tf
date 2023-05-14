e1cef8aa227a93d5b63cd3531a1f388ee33c8c6e
Kacperek0 <kacper.szczepanek@gmail.com>
2023-05-14 12:34:49 +0200

    deploy aks

--- Terraform/modules/acr_aks/main.tf
+++ Terraform/modules/acr_aks/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.prefix}${var.application}${var.environment}acr"
+  name                = "${var.prefix}-${var.application}-${var.environment}-acr"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Premium"
