fba05bddfe196d19f8bc0e5d698467b538b7ecf4
Kacperek0 <47487512+Kacperek0@users.noreply.github.com>
2023-05-14 10:59:48 +0200

    Frontend/change title (#22)
    * update title
    
    * update:

--- Actions/react-app/src/App.jsx
+++ Actions/react-app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>Learn & Master GitHub Actions</h1>
+        <h1>Learn & Master GitHub Actions on CDV</h1>
       </header>
       <MainContent />
     </>
