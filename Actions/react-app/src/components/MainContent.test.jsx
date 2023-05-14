f3a442d733c8271a11ec5d4b2822dfed11afd9bd
Kacperek0 <kacper.szczepanek@gmail.com>
2023-05-14 09:51:04 +0200

    destroy tests

--- Actions/react-app/src/components/MainContent.test.jsx
+++ Actions/react-app/src/components/MainContent.test.jsx
@@ -19,6 +19,6 @@ describe('MainContent', () => {
 
     const button = screen.getByRole('button');
     await userEvent.click(button);
-    expect(screen.getByTestId('help-area')).toBeInTheDocument();
+    expect(screen.getByTestId('help-areaa')).toBeInTheDocument();
   });
 });
