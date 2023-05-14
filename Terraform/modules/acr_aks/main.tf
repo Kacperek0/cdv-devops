aeebf45e16d40e3594bee6cd5bc4dcd7e7962f55
Kacperek0 <kacper.szczepanek@gmail.com>
2023-05-14 12:54:43 +0200

    fix variable

--- Terraform/modules/acr_aks/main.tf
+++ Terraform/modules/acr_aks/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_container_registry" "acr" {
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "{var.prefix}-${var.application}-${var.environment}-aks"
+  name                = "${var.prefix}-${var.application}-${var.environment}-aks"
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = "${var.prefix}${var.application}aks"
